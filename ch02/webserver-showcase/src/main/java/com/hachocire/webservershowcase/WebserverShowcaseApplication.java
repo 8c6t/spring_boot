3b80bb19573747d58aac8527d5af332d49a7210c
8c6t <nkw8620@gmail.com>
2019-10-10 13:49:18 +0900

    2.8.2 내장 웹 서버 응용 2부: HTTP2

--- ch02/webserver-showcase/src/main/java/com/hachocire/webservershowcase/WebserverShowcaseApplication.java
+++ ch02/webserver-showcase/src/main/java/com/hachocire/webservershowcase/WebserverShowcaseApplication.java
@@ -1,11 +1,7 @@
 package com.hachocire.webservershowcase;
 
-import org.apache.catalina.connector.Connector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,7 +18,7 @@ public class WebserverShowcaseApplication {
         SpringApplication.run(WebserverShowcaseApplication.class, args);
     }
 
-    @Bean
+/*    @Bean
     public ServletWebServerFactory serverFactory() {
         TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
         tomcat.addAdditionalTomcatConnectors(createStandardConnector());
@@ -33,6 +29,6 @@ public class WebserverShowcaseApplication {
         Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
         connector.setPort(8080);
         return connector;
-    }
+    }*/
 
 }

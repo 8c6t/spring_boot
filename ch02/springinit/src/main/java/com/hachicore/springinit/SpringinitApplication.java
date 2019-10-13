db3556f47b8986a2301dc3ae7afe9c519512be8d
8c6t <nkw8620@gmail.com>
2019-10-13 19:46:49 +0900

    3.4 외부 설정 1부

--- ch02/springinit/src/main/java/com/hachicore/springinit/SpringinitApplication.java
+++ ch02/springinit/src/main/java/com/hachicore/springinit/SpringinitApplication.java
@@ -1,13 +1,16 @@
 package com.hachicore.springinit;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringinitApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringinitApplication.class, args);
+		SpringApplication application = new SpringApplication(SpringinitApplication.class);
+		application.setWebApplicationType(WebApplicationType.NONE);
+		application.run(args);
 	}
 
 }
--- ch02/springinit/src/test/java/com/hachicore/springinit/SpringinitApplicationTests.java
+++ ch02/springinit/src/test/java/com/hachicore/springinit/SpringinitApplicationTests.java
@@ -2,15 +2,28 @@ package com.hachicore.springinit;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @RunWith(SpringRunner.class)
+// @TestPropertySource(properties = { "hachicore.name=hibiki" })
+//@SpringBootTest(properties = "hachicore.name=paulette")
+@TestPropertySource(locations = "classpath:/test.properties")
 @SpringBootTest
 public class SpringinitApplicationTests {
 
+	@Autowired
+	Environment environment;
+
 	@Test
 	public void contextLoads() {
+		assertThat(environment.getProperty("hachicore.name"))
+				// .isEqualTo("hachicore");
+			.isEqualTo("hibiki");
 	}
-
 }

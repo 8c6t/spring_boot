d391cd4e92394fbad1fed313899dcd35a7d5eacf
8c6t <nkw8620@gmail.com>
2019-10-15 00:24:31 +0900

    3.11 테스트 유틸

--- ch03/springtestdemo/src/test/java/com/hahicore/springtestdemo/sample/SampleWebMvcTest.java
+++ ch03/springtestdemo/src/test/java/com/hahicore/springtestdemo/sample/SampleWebMvcTest.java
@@ -1,13 +1,16 @@
 package com.hahicore.springtestdemo.sample;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.rule.OutputCapture;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -18,6 +21,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringRunner.class)
 @WebMvcTest(SampleController.class)
 public class SampleWebMvcTest {
+
+    @Rule
+    public OutputCapture outputCapture = new OutputCapture();
 
     @MockBean
     SampleService mockSampleService;
@@ -33,6 +39,11 @@ public class SampleWebMvcTest {
                 .andExpect(status().isOk())
                 .andExpect(content().string("hello rokuthread"))
                 .andDo(print());
+
+
+        assertThat(outputCapture.toString())
+                .contains("hibiki")
+                .contains("skip");
     }
 
 }

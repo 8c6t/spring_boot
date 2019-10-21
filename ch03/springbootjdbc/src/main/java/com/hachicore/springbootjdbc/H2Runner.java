8e36c0343f3d570961beee953e7d7e1284d005a7
8c6t <nkw8620@gmail.com>
2019-10-21 16:46:29 +0900

    3.25 스프링 데이터 2부: 인메모리 DB

---
+++ ch03/springbootjdbc/src/main/java/com/hachicore/springbootjdbc/H2Runner.java
@@ -0,0 +1,40 @@
+package com.hachicore.springbootjdbc;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.Statement;
+
+@Component
+public class H2Runner implements ApplicationRunner {
+
+    @Autowired
+    DataSource dataSource;
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    private Logger logger = LoggerFactory.getLogger(H2Runner.class);
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        try(Connection connection = dataSource.getConnection()) {
+            logger.info(connection.getMetaData().getURL());
+            logger.info(connection.getMetaData().getUserName());
+
+            Statement statement = connection.createStatement();
+            String sql = "CREATE TABLE USER(ID INTEGER NOT NULL, name VARCHAR(255), PRIMARY KEY (id))";
+            statement.executeUpdate(sql);
+        }
+
+        jdbcTemplate.execute("INSERT INTO USER VALUES (1, 'hachicore')");
+
+    }
+}

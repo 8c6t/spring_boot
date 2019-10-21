00af51721abeee759f9c0884b2c203e0a71607af
8c6t <nkw8620@gmail.com>
2019-10-21 17:48:24 +0900

    3.26 스프링 데이터 3부: MySQL

--- ch03/springbootjdbc/src/main/java/com/hachicore/springbootjdbc/MySQLRunner.java
+++ ch03/springbootjdbc/src/main/java/com/hachicore/springbootjdbc/MySQLRunner.java
@@ -10,10 +10,9 @@ import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
-import java.sql.Statement;
 
 @Component
-public class H2Runner implements ApplicationRunner {
+public class MySQLRunner implements ApplicationRunner {
 
     @Autowired
     DataSource dataSource;
@@ -21,7 +20,7 @@ public class H2Runner implements ApplicationRunner {
     @Autowired
     JdbcTemplate jdbcTemplate;
 
-    private Logger logger = LoggerFactory.getLogger(H2Runner.class);
+    private Logger logger = LoggerFactory.getLogger(MySQLRunner.class);
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
@@ -29,9 +28,9 @@ public class H2Runner implements ApplicationRunner {
             logger.info(connection.getMetaData().getURL());
             logger.info(connection.getMetaData().getUserName());
 
-            Statement statement = connection.createStatement();
-            String sql = "CREATE TABLE USER(ID INTEGER NOT NULL, name VARCHAR(255), PRIMARY KEY (id))";
-            statement.executeUpdate(sql);
+//            Statement statement = connection.createStatement();
+//            String sql = "CREATE TABLE USER(ID INTEGER NOT NULL, name VARCHAR(255), PRIMARY KEY (id))";
+//            statement.executeUpdate(sql);
         }
 
         jdbcTemplate.execute("INSERT INTO USER VALUES (1, 'hachicore')");

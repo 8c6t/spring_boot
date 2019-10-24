7d9fa34cb4afd3b6f71dec5ab2127ec8e89a1c1b
8c6t <nkw8620@gmail.com>
2019-10-25 02:38:28 +0900

    4.3 스프링 부트 Actuator 3부: 스프링 부트 어드민

---
+++ ch04/demospringmonitor/src/test/java/com/hachicore/demospringmonitor/DemospringmonitorApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hachicore.demospringmonitor;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemospringmonitorApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
